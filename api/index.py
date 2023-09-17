a900e1cc5fd3b81113cc8564eef98e477bdd5067
S-P-H-E <sphe.g.personal@gmail.com>
2023-09-17 08:18:18 +0200

    update

--- api/index.py
+++ api/index.py
@@ -1,10 +1,66 @@
 from http.server import BaseHTTPRequestHandler
+import os
+import discord
+import openai
+from discord.ext import commands
+from dotenv import load_dotenv
+
+# Load environment variables from .env.local
+load_dotenv(".env.local")
+
+# Retrieve API keys from environment variables
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+intents = discord.Intents.default()  # Enable default intents
+intents.typing = False  # You can adjust intents based on your bot's functionality
+
+# Set up the Discord bot
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Configure OpenAI
+openai.api_key = OPENAI_API_KEY
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world mate!'.encode('utf-8'))
+        response_message = f'Logged in as {bot.user.name}'.encode('utf-8')
+        self.wfile.write(response_message)
         return
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if isinstance(message.channel, discord.DMChannel):
+        question = message.content
+        response = generate_openai_response(question)
+        await message.author.send(response)
+    elif isinstance(message.channel, discord.TextChannel):
+        if message.content.startswith('!ask'):
+            question = message.content[5:]  # Remove the !ask part
+            response = generate_openai_response(question)
+            code_response = f'``` {response} ```'
+            await message.channel.send(code_response)
+
+    await bot.process_commands(message)
+
+def generate_openai_response(question):
+    prompt = f"Question: {question}\nAnswer:"
+    response = openai.Completion.create(
+        engine="text-davinci-003",  # Choose the OpenAI engine you prefer
+        prompt=prompt,
+        max_tokens=50  # Adjust the response length as needed
+    )
+    return response.choices[0].text.strip()
+
+bot.run(DISCORD_TOKEN)
