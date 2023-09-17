3d36f48f07072df27439baf1ffd5395f38bc7642
S-P-H-E <sphe.g.personal@gmail.com>
2023-09-17 08:50:00 +0200

    flask+

--- api/index.py
+++ api/index.py
@@ -1,11 +1,33 @@
 from flask import Flask
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
+load_dotenv(".env.local")
+
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()  # Enable default intents
+intents.typing = False
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+@bot.command(name='hello')
+async def hello(ctx):
+    # Respond with "hello" when the "!hello" command is invoked
+    await ctx.send('hello')
+
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return f'Logged in as {bot.user.name}'
 
 @app.route('/about')
 def about():
-    return 'About'
+    return 'I AM A BOT'
